6540c7d5759b3ecb6bbb0406de4ba182e4a04c30
winebarrel <sugawara@winebarrel.jp>
2023-02-13 01:20:22 +0900

    Add get status

---
+++ status.go
@@ -0,0 +1,67 @@
+package redash
+
+import (
+	"context"
+
+	"github.com/winebarrel/redash-go/internal/util"
+)
+
+type Status struct {
+	DashboardsCount         int                   `json:"dashboards_count"`
+	DatabaseMetrics         StatusDatabaseMetrics `json:"database_metrics"`
+	Manager                 StatusManager         `json:"manager"`
+	QueriesCount            int                   `json:"queries_count"`
+	QueryResultsCount       int                   `json:"query_results_count"`
+	RedisUsedMemory         int                   `json:"redis_used_memory"`
+	RedisUsedMemoryHuman    string                `json:"redis_used_memory_human"`
+	UnusedQueryResultsCount int                   `json:"unused_query_results_count"`
+	Version                 string                `json:"version"`
+	WidgetsCount            int                   `json:"widgets_count"`
+	Workers                 []any                 `json:"workers"`
+}
+
+type StatusDatabaseMetrics struct {
+	Metrics [][]any `json:"metrics"`
+}
+
+type StatusManager struct {
+	LastRefreshAt        string              `json:"last_refresh_at"`
+	OutdatedQueriesCount string              `json:"outdated_queries_count"`
+	QueryIds             string              `json:"query_ids"`
+	Queues               StatusManagerQueues `json:"queues"`
+}
+
+type StatusManagerQueues struct {
+	Celery           StatusManagerQueuesCelery           `json:"celery"`
+	Queries          StatusManagerQueuesQueries          `json:"queries"`
+	ScheduledQueries StatusManagerQueuesScheduledQueries `json:"scheduled_queries"`
+}
+
+type StatusManagerQueuesCelery struct {
+	Size int `json:"size"`
+}
+
+type StatusManagerQueuesQueries struct {
+	Size int `json:"size"`
+}
+
+type StatusManagerQueuesScheduledQueries struct {
+	Size int `json:"size"`
+}
+
+func (client *Client) GetStatus(ctx context.Context) (*Status, error) {
+	res, close, err := client.Get(ctx, "status.json", nil)
+	defer close()
+
+	if err != nil {
+		return nil, err
+	}
+
+	status := &Status{}
+
+	if err := util.UnmarshalBody(res, &status); err != nil {
+		return nil, err
+	}
+
+	return status, nil
+}
---
+++ status_test.go
@@ -0,0 +1,120 @@
+package redash_test
+
+import (
+	"context"
+	"net/http"
+	"testing"
+
+	"github.com/jarcoal/httpmock"
+	"github.com/stretchr/testify/assert"
+	"github.com/winebarrel/redash-go"
+)
+
+func Test_GetStatus_OK(t *testing.T) {
+	assert := assert.New(t)
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder(http.MethodGet, "https://redash.example.com/status.json", func(req *http.Request) (*http.Response, error) {
+		assert.Equal(
+			http.Header(
+				http.Header{
+					"Authorization": []string{"Key " + testRedashAPIKey},
+					"Content-Type":  []string{"application/json"},
+					"User-Agent":    []string{"redash-go"},
+				},
+			),
+			req.Header,
+		)
+		return httpmock.NewStringResponse(http.StatusOK, `
+			{
+				"dashboards_count": 2,
+				"database_metrics": {
+					"metrics": [
+						[
+							"Query Results Size",
+							49152
+						],
+						[
+							"Redash DB Size",
+							9371820
+						]
+					]
+				},
+				"manager": {
+					"last_refresh_at": "1676217265.221729",
+					"outdated_queries_count": "0",
+					"query_ids": "[]",
+					"queues": {
+						"celery": {
+							"size": 0
+						},
+						"queries": {
+							"size": 0
+						},
+						"scheduled_queries": {
+							"size": 0
+						}
+					}
+				},
+				"queries_count": 5,
+				"query_results_count": 4,
+				"redis_used_memory": 2383280,
+				"redis_used_memory_human": "2.27M",
+				"unused_query_results_count": 0,
+				"version": "8.0.0+b32245",
+				"widgets_count": 0,
+				"workers": []
+			}
+		`), nil
+	})
+
+	client, _ := redash.NewClient("https://redash.example.com", testRedashAPIKey)
+	res, err := client.GetStatus(context.Background())
+	assert.NoError(err)
+	assert.Equal(&redash.Status{
+		DashboardsCount: 2,
+		DatabaseMetrics: redash.StatusDatabaseMetrics{
+			Metrics: [][]any{
+				{"Query Results Size", float64(49152)},
+				{"Redash DB Size", 9.37182e+06},
+			},
+		},
+		Manager: redash.StatusManager{
+			LastRefreshAt:        "1676217265.221729",
+			OutdatedQueriesCount: "0",
+			QueryIds:             "[]",
+			Queues: redash.StatusManagerQueues{
+				Celery: redash.StatusManagerQueuesCelery{
+					Size: 0,
+				},
+				Queries: redash.StatusManagerQueuesQueries{
+					Size: 0,
+				},
+				ScheduledQueries: redash.StatusManagerQueuesScheduledQueries{
+					Size: 0,
+				},
+			},
+		},
+		QueriesCount:            5,
+		QueryResultsCount:       4,
+		RedisUsedMemory:         2383280,
+		RedisUsedMemoryHuman:    "2.27M",
+		UnusedQueryResultsCount: 0,
+		Version:                 "8.0.0+b32245",
+		WidgetsCount:            0,
+		Workers:                 []any{},
+	}, res)
+}
+
+func Test_GetStatus_Acc(t *testing.T) {
+	if !testAcc {
+		t.Skip()
+	}
+
+	assert := assert.New(t)
+	client, _ := redash.NewClient(testRedashEndpoint, testRedashAPIKey)
+	status, err := client.GetStatus(context.Background())
+	assert.NoError(err)
+	assert.NotEmpty(status.Version)
+}
