8b878047df613dfac1db0690e0900b8cf4c5c4b3
winebarrel <sugawara@winebarrel.jp>
2023-02-25 22:29:47 +0900

    Add UpdateSettingsOrganization

--- settings.go
+++ settings.go
@@ -12,36 +12,53 @@ type SettingsOrganization struct {
 }
 
 type SettingsOrganizationSettings struct {
-	AuthGoogleAppsDomains             []string `json:"auth_google_apps_domains"`
-	AuthJwtAuthAlgorithms             []string `json:"auth_jwt_auth_algorithms"`
-	AuthJwtAuthAudience               string   `json:"auth_jwt_auth_audience"`
-	AuthJwtAuthCookieName             string   `json:"auth_jwt_auth_cookie_name"`
-	AuthJwtAuthHeaderName             string   `json:"auth_jwt_auth_header_name"`
-	AuthJwtAuthIssuer                 string   `json:"auth_jwt_auth_issuer"`
-	AuthJwtAuthPublicCertsURL         string   `json:"auth_jwt_auth_public_certs_url"`
-	AuthJwtLoginEnabled               bool     `json:"auth_jwt_login_enabled"`
-	AuthPasswordLoginEnabled          bool     `json:"auth_password_login_enabled"`
-	AuthSamlEnabled                   bool     `json:"auth_saml_enabled"`
-	AuthSamlEntityID                  string   `json:"auth_saml_entity_id"`
-	AuthSamlMetadataURL               string   `json:"auth_saml_metadata_url"`
-	AuthSamlNameidFormat              string   `json:"auth_saml_nameid_format"`
-	AuthSamlSsoURL                    string   `json:"auth_saml_sso_url"`
-	AuthSamlType                      string   `json:"auth_saml_type"`
-	AuthSamlX509Cert                  string   `json:"auth_saml_x509_cert"`
-	BeaconConsent                     bool     `json:"beacon_consent"`
-	DateFormat                        string   `json:"date_format"`
-	DisablePublicUrls                 bool     `json:"disable_public_urls"`
-	FeatureShowPermissionsControl     bool     `json:"feature_show_permissions_control"`
-	FloatFormat                       string   `json:"float_format"`
-	HidePlotlyModeBar                 bool     `json:"hide_plotly_mode_bar"`
-	IntegerFormat                     string   `json:"integer_format"`
-	MultiByteSearchEnabled            bool     `json:"multi_byte_search_enabled"`
-	SendEmailOnFailedScheduledQueries bool     `json:"send_email_on_failed_scheduled_queries"`
-	TimeFormat                        string   `json:"time_format"`
+	AuthGoogleAppsDomains             []string `json:"auth_google_apps_domains,omitempty"`
+	AuthJwtAuthAlgorithms             []string `json:"auth_jwt_auth_algorithms,omitempty"`
+	AuthJwtAuthAudience               string   `json:"auth_jwt_auth_audience,omitempty"`
+	AuthJwtAuthCookieName             string   `json:"auth_jwt_auth_cookie_name,omitempty"`
+	AuthJwtAuthHeaderName             string   `json:"auth_jwt_auth_header_name,omitempty"`
+	AuthJwtAuthIssuer                 string   `json:"auth_jwt_auth_issuer,omitempty"`
+	AuthJwtAuthPublicCertsURL         string   `json:"auth_jwt_auth_public_certs_url,omitempty"`
+	AuthJwtLoginEnabled               bool     `json:"auth_jwt_login_enabled,omitempty"`
+	AuthPasswordLoginEnabled          bool     `json:"auth_password_login_enabled,omitempty"`
+	AuthSamlEnabled                   bool     `json:"auth_saml_enabled,omitempty"`
+	AuthSamlEntityID                  string   `json:"auth_saml_entity_id,omitempty"`
+	AuthSamlMetadataURL               string   `json:"auth_saml_metadata_url,omitempty"`
+	AuthSamlNameidFormat              string   `json:"auth_saml_nameid_format,omitempty"`
+	AuthSamlSsoURL                    string   `json:"auth_saml_sso_url,omitempty"`
+	AuthSamlType                      string   `json:"auth_saml_type,omitempty"`
+	AuthSamlX509Cert                  string   `json:"auth_saml_x509_cert,omitempty"`
+	BeaconConsent                     bool     `json:"beacon_consent,omitempty"`
+	DateFormat                        string   `json:"date_format,omitempty"`
+	DisablePublicUrls                 bool     `json:"disable_public_urls,omitempty"`
+	FeatureShowPermissionsControl     bool     `json:"feature_show_permissions_control,omitempty"`
+	FloatFormat                       string   `json:"float_format,omitempty"`
+	HidePlotlyModeBar                 bool     `json:"hide_plotly_mode_bar,omitempty"`
+	IntegerFormat                     string   `json:"integer_format,omitempty"`
+	MultiByteSearchEnabled            bool     `json:"multi_byte_search_enabled,omitempty"`
+	SendEmailOnFailedScheduledQueries bool     `json:"send_email_on_failed_scheduled_queries,omitempty"`
+	TimeFormat                        string   `json:"time_format,omitempty"`
 }
 
 func (client *Client) GetSettingsOrganization(ctx context.Context) (*SettingsOrganization, error) {
 	res, close, err := client.Get(ctx, "api/settings/organization", nil)
+	defer close()
+
+	if err != nil {
+		return nil, err
+	}
+
+	org := &SettingsOrganization{}
+
+	if err := util.UnmarshalBody(res, &org); err != nil {
+		return nil, err
+	}
+
+	return org, nil
+}
+
+func (client *Client) UpdateSettingsOrganization(ctx context.Context, input *SettingsOrganizationSettings) (*SettingsOrganization, error) {
+	res, close, err := client.Post(ctx, "api/settings/organization", input)
 	defer close()
 
 	if err != nil {
--- settings_test.go
+++ settings_test.go
@@ -2,6 +2,7 @@ package redash_test
 
 import (
 	"context"
+	"io"
 	"net/http"
 	"testing"
 
@@ -99,6 +100,102 @@ func Test_GetSettingsOrganization_OK(t *testing.T) {
 	}, res)
 }
 
+func Test_UpdateSettingsOrganization_OK(t *testing.T) {
+	assert := assert.New(t)
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder(http.MethodPost, "https://redash.example.com/api/settings/organization", func(req *http.Request) (*http.Response, error) {
+		assert.Equal(
+			http.Header(
+				http.Header{
+					"Authorization": []string{"Key " + testRedashAPIKey},
+					"Content-Type":  []string{"application/json"},
+					"User-Agent":    []string{"redash-go"},
+				},
+			),
+			req.Header,
+		)
+		if req.Body == nil {
+			assert.FailNow("req.Body is nil")
+		}
+		body, _ := io.ReadAll(req.Body)
+		assert.Equal(`{"date_format":"YYYY/MM/DD"}`, string(body))
+		return httpmock.NewStringResponse(http.StatusOK, `
+			{
+				"settings": {
+					"auth_google_apps_domains": [],
+					"auth_jwt_auth_algorithms": [
+						"HS256",
+						"RS256",
+						"ES256"
+					],
+					"auth_jwt_auth_audience": "",
+					"auth_jwt_auth_cookie_name": "",
+					"auth_jwt_auth_header_name": "",
+					"auth_jwt_auth_issuer": "",
+					"auth_jwt_auth_public_certs_url": "",
+					"auth_jwt_login_enabled": false,
+					"auth_password_login_enabled": true,
+					"auth_saml_enabled": false,
+					"auth_saml_entity_id": "",
+					"auth_saml_metadata_url": "",
+					"auth_saml_nameid_format": "",
+					"auth_saml_sso_url": "",
+					"auth_saml_type": "",
+					"auth_saml_x509_cert": "",
+					"beacon_consent": false,
+					"date_format": "YYYY/MM/DD",
+					"disable_public_urls": false,
+					"feature_show_permissions_control": false,
+					"float_format": "0,0.00",
+					"hide_plotly_mode_bar": false,
+					"integer_format": "0,0",
+					"multi_byte_search_enabled": false,
+					"send_email_on_failed_scheduled_queries": false,
+					"time_format": "HH:mm"
+				}
+			}
+		`), nil
+	})
+
+	client, _ := redash.NewClient("https://redash.example.com", testRedashAPIKey)
+	res, err := client.UpdateSettingsOrganization(context.Background(), &redash.SettingsOrganizationSettings{
+		DateFormat: "YYYY/MM/DD",
+	})
+	assert.NoError(err)
+	assert.Equal(&redash.SettingsOrganization{
+		SettingsOrganizationSettings: redash.SettingsOrganizationSettings{
+			AuthGoogleAppsDomains:             []string{},
+			AuthJwtAuthAlgorithms:             []string{"HS256", "RS256", "ES256"},
+			AuthJwtAuthAudience:               "",
+			AuthJwtAuthCookieName:             "",
+			AuthJwtAuthHeaderName:             "",
+			AuthJwtAuthIssuer:                 "",
+			AuthJwtAuthPublicCertsURL:         "",
+			AuthJwtLoginEnabled:               false,
+			AuthPasswordLoginEnabled:          true,
+			AuthSamlEnabled:                   false,
+			AuthSamlEntityID:                  "",
+			AuthSamlMetadataURL:               "",
+			AuthSamlNameidFormat:              "",
+			AuthSamlSsoURL:                    "",
+			AuthSamlType:                      "",
+			AuthSamlX509Cert:                  "",
+			BeaconConsent:                     false,
+			DateFormat:                        "YYYY/MM/DD",
+			DisablePublicUrls:                 false,
+			FeatureShowPermissionsControl:     false,
+			FloatFormat:                       "0,0.00",
+			HidePlotlyModeBar:                 false,
+			IntegerFormat:                     "0,0",
+			MultiByteSearchEnabled:            false,
+			SendEmailOnFailedScheduledQueries: false,
+			TimeFormat:                        "HH:mm",
+		},
+	}, res)
+}
+
 func Test_Settings_Acc(t *testing.T) {
 	if !testAcc {
 		t.Skip()
@@ -108,5 +205,11 @@ func Test_Settings_Acc(t *testing.T) {
 	client, _ := redash.NewClient(testRedashEndpoint, testRedashAPIKey)
 	settings, err := client.GetSettingsOrganization(context.Background())
 	assert.NoError(err)
-	assert.Equal("DD/MM/YY", settings.SettingsOrganizationSettings.DateFormat)
+	assert.NotEmpty(settings.SettingsOrganizationSettings.DateFormat)
+
+	settings, err = client.UpdateSettingsOrganization(context.Background(), &redash.SettingsOrganizationSettings{
+		DateFormat: "YYYY/MM/DD",
+	})
+	assert.NoError(err)
+	assert.Equal("YYYY/MM/DD", settings.SettingsOrganizationSettings.DateFormat)
 }
