8f65d9910860fc3c7faf821fd5e8efcdaac1150d
winebarrel <sugawara@winebarrel.jp>
2023-04-25 19:05:39 +0900

    Add Client.UpdateUser()

--- user_test.go
+++ user_test.go
@@ -229,6 +229,74 @@ func Test_CreateUser_OK(t *testing.T) {
 	}, res)
 }
 
+func Test_UpdateUser_OK(t *testing.T) {
+	assert := assert.New(t)
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder(http.MethodPost, "https://redash.example.com/api/users/1", func(req *http.Request) (*http.Response, error) {
+		assert.Equal(
+			http.Header(
+				http.Header{
+					"Authorization": []string{"Key " + testRedashAPIKey},
+					"Content-Type":  []string{"application/json"},
+					"User-Agent":    []string{"redash-go"},
+				},
+			),
+			req.Header,
+		)
+		if req.Body == nil {
+			assert.FailNow("req.Body is nil")
+		}
+		body, _ := io.ReadAll(req.Body)
+		assert.Equal(`{"email":"admin2@example.com","name":"admin2"}`, string(body))
+		return httpmock.NewStringResponse(http.StatusOK, `
+			{
+				"active_at": "2023-02-10T01:23:45.000Z",
+				"api_key": "api_key",
+				"auth_type": "password",
+				"created_at": "2023-02-10T01:23:45.000Z",
+				"disabled_at": null,
+				"email": "admin2@example.com",
+				"groups": [
+					1,
+					2
+				],
+				"id": 1,
+				"is_disabled": false,
+				"is_email_verified": true,
+				"is_invitation_pending": false,
+				"name": "admin2",
+				"profile_image_url": "https://www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=40&d=identicon",
+				"updated_at": "2023-02-10T01:23:45.000Z"
+			}
+		`), nil
+	})
+
+	client, _ := redash.NewClient("https://redash.example.com", testRedashAPIKey)
+	res, err := client.UpdateUser(context.Background(), 1, &redash.UpdateUserInput{
+		Email: "admin2@example.com",
+		Name:  "admin2",
+	})
+	assert.NoError(err)
+	assert.Equal(&redash.User{
+		ActiveAt:            dateparse.MustParse("2023-02-10T01:23:45.000Z"),
+		APIKey:              "api_key",
+		AuthType:            "password",
+		CreatedAt:           dateparse.MustParse("2023-02-10T01:23:45.000Z"),
+		DisabledAt:          time.Time{},
+		Email:               "admin2@example.com",
+		Groups:              []any{float64(1), float64(2)},
+		ID:                  1,
+		IsDisabled:          false,
+		IsEmailVerified:     true,
+		IsInvitationPending: false,
+		Name:                "admin2",
+		ProfileImageURL:     "https://www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=40&d=identicon",
+		UpdatedAt:           dateparse.MustParse("2023-02-10T01:23:45.000Z"),
+	}, res)
+}
+
 func Test_DeleteUser_OK(t *testing.T) {
 	assert := assert.New(t)
 	httpmock.Activate()
@@ -382,7 +450,8 @@ func Test_User_Acc(t *testing.T) {
 	client, _ := redash.NewClient(testRedashEndpoint, testRedashAPIKey)
 
 	uuidObj, _ := uuid.NewUUID()
-	email := uuidObj.String() + "@example.com"
+	name := uuidObj.String()
+	email := name + "@example.com"
 
 	_, err := client.ListUsers(context.Background(), nil)
 	assert.NoError(err)
@@ -390,23 +459,37 @@ func Test_User_Acc(t *testing.T) {
 	user, err := client.CreateUser(context.Background(), &redash.CreateUsersInput{
 		AuthType: "password",
 		Email:    email,
-		Name:     uuidObj.String(),
+		Name:     name,
 	})
 	assert.NoError(err)
 	assert.Equal(email, user.Email)
 
 	user, err = client.GetUser(context.Background(), user.ID)
 	assert.NoError(err)
+	assert.Equal(name, user.Name)
 	assert.Equal(email, user.Email)
+
+	newName := name + "2"
+	newEmail := email + ".uk"
+
+	user, err = client.UpdateUser(context.Background(), user.ID, &redash.UpdateUserInput{
+		Email: newEmail,
+		Name:  newName,
+	})
+	assert.NoError(err)
+	assert.Equal(newName, user.Name)
+	assert.Equal(newEmail, user.Email)
 
 	user, err = client.DisableUser(context.Background(), user.ID)
 	assert.NoError(err)
-	assert.Equal(email, user.Email)
+	assert.Equal(newName, user.Name)
+	assert.Equal(newEmail, user.Email)
 	assert.True(user.IsDisabled)
 
 	user, err = client.EnableUser(context.Background(), user.ID)
 	assert.NoError(err)
-	assert.Equal(email, user.Email)
+	assert.Equal(newName, user.Name)
+	assert.Equal(newEmail, user.Email)
 	assert.False(user.IsDisabled)
 
 	err = client.DeleteUser(context.Background(), user.ID)
